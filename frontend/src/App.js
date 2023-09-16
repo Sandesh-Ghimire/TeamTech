963470a8e98875874f8643d0f58d1692a0a43956
sandesh-ghimire <ghimiresandesh45@gmail.com>
2023-09-16 13:49:54 +0530

    ready to upload login and signup with  backend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { LoginReg } from './Components/LoginReg/LoginReg';
+
 import { Space } from './Components/Space/Space';
 import { User } from './Components/User/User';
 import { Payment } from './Components/Payment/Payment';
@@ -8,8 +8,8 @@ import { Payment } from './Components/Payment/Payment';
 function App() {
   return (
     <div>
-<LoginReg/>
-{/* <Payment/> */}
+{/* <LoginReg/> */}
+<Payment/>
     </div>
 );
 }
