3f02a19298ca74bdc0a6168d3990a41f96053f9f
sandesh-ghimire <ghimiresandesh45@gmail.com>
2023-09-16 15:18:45 +0530

    payment sample added.

--- frontend/src/Components/Payment/Payment.jsx
+++ frontend/src/Components/Payment/Payment.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react' //importing necessary libraries and dependency
 import './Payment.css'
 import qr from './qr.png'
-
+import esewa from './esewa1.png'
+import khalti from './Khalti.png'
 export const Payment = () => {
 
 
@@ -9,19 +10,23 @@ export const Payment = () => {
     <div>
      
     
-      <div className="container">
+      <div className="container1">
     
     <div className='ads'>
       <h2>Your Favorite Payment System</h2>
      <img className='qr' src={qr} alt="" />
+
+     <img className='esewa' src={esewa} alt="" />
+     <img className='khalti' src={khalti} alt="" />
     </div>
+
 
    
   
        
       </div>
       
-   
+     
 
     </div>
   )
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -6,6 +6,7 @@ import {User} from './Components/User/User'
 import { Space } from './Components/Space/Space'
 import PrivateRoutes from './auth/PrivateRoutes'
 import AdminRoutes from './auth/AdminRoutes'
+import { Payment } from './Components/Payment/Payment'
 
 function Routes() {
   return (
@@ -13,9 +14,10 @@ function Routes() {
     <ReactRouts>
         <Route path='/' element={<SignUP/>}/>
         <Route path='/login' element={<LoginReg/>}/>
-        <Route element={<PrivateRoutes/>}>
+        {/* <Route element={<PrivateRoutes/>}> */}
           <Route path='/rent' element={<User/>}/>
-        </Route>
+          <Route path='/Payment' element={<Payment/>}/>
+        {/* </Route> */}
         <Route element={<AdminRoutes/>}>
         <Route path='/space' element={<Space/>}/>
         </Route>
