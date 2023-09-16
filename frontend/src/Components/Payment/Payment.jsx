637b6751cf73021affe8785dadb8f124448ef424
smabid2018 <123981404+smabid2018@users.noreply.github.com>
2023-09-16 18:37:22 +0545

    Update Payment.jsx

--- frontend/src/Components/Payment/Payment.jsx
+++ frontend/src/Components/Payment/Payment.jsx
@@ -3,31 +3,18 @@ import './Payment.css'
 import qr from './qr.png'
 import esewa from './esewa1.png'
 import khalti from './Khalti.png'
+
 export const Payment = () => {
-
-
   return (
     <div>
-     
-    
       <div className="container1">
-    
     <div className='ads'>
       <h2>Your Favorite Payment System</h2>
      <img className='qr' src={qr} alt="" />
-
      <img className='esewa' src={esewa} alt="" />
      <img className='khalti' src={khalti} alt="" />
     </div>
-
-
-   
-  
-       
       </div>
-      
-     
-
     </div>
   )
 }
