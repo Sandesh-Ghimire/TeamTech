793c6c33d3ec87b3c6f8f05dd6d7ad05a903dddb
sandesh-ghimire <ghimiresandesh45@gmail.com>
2023-09-16 21:23:50 +0530

    map functionality is fully awake.

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,19 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 
-app.use(express.json())
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', '*');
+//     res.header('Access-Control-Request-Headers', '*');
+//     if (req.method === "OPTIONS") {
+//       res.header('Access-Control-Allow-Methods', '*');
+//       return res.status(200).json({});
+//     }
+//     next();
+//   });
+
 app.use(cors())
+app.use(express.json())
 app.use(cookieParser())
 
 const connectDB = require('./connect')
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,12 +6,12 @@ const { check } = require('express-validator');
 const {signin,signup,signout,isSignedIn} = require('./../controller/auth')
 
 authRoutes.post('/signin',
-    check('email',"Enter the valid email").isEmail(),
+    
+    check('username',"Name should be 3 characters long").isLength({min:3}),
+    check('email',"Enter the valid email").isEmail(),check('email',"Enter the valid email").isEmail(),
     check('password',"Password should be 8 charactrs long").isLength({min:8})
     ,signin)
 authRoutes.post('/signup',
-    check('username',"Name should be 3 characters long").isLength({min:3}),
-    check('email',"Enter the valid email").isEmail(),
     check('password',"Password should be 8 charactrs long").isLength({min:8})
     ,signup)
 authRoutes.get('/signout',signout)
